33e23e21314dad94621dbee63bedf90d7ed84360
Mathias Gumpen Gundersen <90856007+MathiasGuGu@users.noreply.github.com>
2023-03-01 11:21:29 +0100

    Delete CaAPI directory

--- CaAPI/js/api_auth.js
+++
@@ -1,6 +0,0 @@
-export const API_KEY_V3 = "fc78d1f8b22cfa98b46f5555c25259d0";
-export const READ_ACCESS_TOKEN =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYzc4ZDFmOGIyMmNmYTk4YjQ2ZjU1NTVjMjUyNTlkMCIsInN1YiI6IjYzZjRkMGI3NTI0OTc4MDA4NGY4MWQyNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PzL7Fo5pl3kD7ouKn6wuZkDkT3twYfAUfqIOBMynMmg";
-export const EXAMPLE_REQ =
-  "https://api.themoviedb.org/3/movie/550?api_key=fc78d1f8b22cfa98b46f5555c25259d0";
-export const API_URL = "https://api.themoviedb.org/3/";
